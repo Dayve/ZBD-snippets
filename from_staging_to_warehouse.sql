a5cedef235725e187398ee8370dcf52e6f5cb08f
jsekulowicz <jan.sekulowicz@codesthq.com>
2017-11-25 23:29:59 +0100

    New staging diagram/db. Added some staging -> warehouse etl.

--- from_staging_to_warehouse.sql
+++ from_staging_to_warehouse.sql
@@ -18,7 +18,7 @@ BEGIN
 
     RETURN zwracany_id;
 END;
-/
+
 
 -- Dostawcy:
 INSERT INTO zbd_warehouse.dostawca (id, nazwa)
@@ -40,6 +40,21 @@ INSERT INTO zbd_warehouse.promocja (id, przedzial_procentowy_obnizki)
 SELECT id, obniżka_procentowa
 FROM zbd_staging.promocje;
 
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (1, '0 - 5%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (2, '5 - 10%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (2, '10 - 15%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (3, '15 - 20%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (4, '20 - 25%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (5, '25 - 30%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (6, '30 - 35%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (7, '35 - 40%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (8, '40 - 50%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (9, '50 - 60%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (10, '60 - 70%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (11, '70 - 80%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (12, '80 - 90%');
+INSERT INTO zbd_warehouse.promocja (id, przedzial_kwoty_sumarycznej) VALUES (13, '90 - 100%');
+
 -- Reklamacje:
 INSERT INTO zbd_warehouse.reklamacja (id, powod)
 SELECT id, powód
@@ -55,6 +70,32 @@ INSERT INTO zbd_warehouse.produkt (id, nazwa)
 SELECT id, nazwa
 FROM zbd_staging.produkt;
 
+-- Wielkość Transakcji:
+INSERT INTO zbd_warehouse.wielkosc_transakcji (id, przedzial_kwoty_sumarycznej) VALUES (1, '0 - 10zł');
+INSERT INTO zbd_warehouse.wielkosc_transakcji (id, przedzial_kwoty_sumarycznej) VALUES (2, '10 - 30zł');
+INSERT INTO zbd_warehouse.wielkosc_transakcji (id, przedzial_kwoty_sumarycznej) VALUES (2, '30 - 50zł');
+INSERT INTO zbd_warehouse.wielkosc_transakcji (id, przedzial_kwoty_sumarycznej) VALUES (3, '50 - 150zł');
+INSERT INTO zbd_warehouse.wielkosc_transakcji (id, przedzial_kwoty_sumarycznej) VALUES (4, '150 - 300 zł');
+INSERT INTO zbd_warehouse.wielkosc_transakcji (id, przedzial_kwoty_sumarycznej) VALUES (5, '300 - 500 zł');
+INSERT INTO zbd_warehouse.wielkosc_transakcji (id, przedzial_kwoty_sumarycznej) VALUES (6, '500+ zł');
+
+-- Przedział kosztu zamówienia:
+INSERT INTO zbd_warehouse.przedzial_kosztu_zamowienia (id, przedzial) VALUES (1, '500 - 1000zł');
+INSERT INTO zbd_warehouse.przedzial_kosztu_zamowienia (id, przedzial) VALUES (2, '1000 - 3000zł');
+INSERT INTO zbd_warehouse.przedzial_kosztu_zamowienia (id, przedzial) VALUES (2, '3000 - 5000zł');
+INSERT INTO zbd_warehouse.przedzial_kosztu_zamowienia (id, przedzial) VALUES (3, '5000 - 15000zł');
+INSERT INTO zbd_warehouse.przedzial_kosztu_zamowienia (id, przedzial) VALUES (4, '15000 - 30000 zł');
+INSERT INTO zbd_warehouse.przedzial_kosztu_zamowienia (id, przedzial) VALUES (5, '30000 - 50000 zł');
+INSERT INTO zbd_warehouse.przedzial_kosztu_zamowienia (id, przedzial) VALUES (6, '50000+ zł');
+
+--Kategoria cenowa:
+INSERT INTO zbd_warehouse.kategoria_cenowa (id, przedzial_cenowy) VALUES (1, '0 - 10zł');
+INSERT INTO zbd_warehouse.kategoria_cenowa (id, przedzial_cenowy) VALUES (2, '10 - 30zł');
+INSERT INTO zbd_warehouse.kategoria_cenowa (id, przedzial_cenowy) VALUES (2, '30 - 50zł');
+INSERT INTO zbd_warehouse.kategoria_cenowa (id, przedzial_cenowy) VALUES (3, '50 - 150zł');
+INSERT INTO zbd_warehouse.kategoria_cenowa (id, przedzial_cenowy) VALUES (4, '150 - 300 zł');
+INSERT INTO zbd_warehouse.kategoria_cenowa (id, przedzial_cenowy) VALUES (5, '300 - 500 zł');
+INSERT INTO zbd_warehouse.kategoria_cenowa (id, przedzial_cenowy) VALUES (6, '500+ zł');
 
 -- Działy i kategorie - Typ produktu:
 CREATE OR REPLACE PROCEDURE dodaj_typy_produktow
--- staging/staging_zbd_drop.sql
+++ staging/staging_zbd_drop.sql
@@ -1,6 +1,3 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2017-11-22 18:18:13.832
-
 -- foreign keys
 ALTER TABLE Cena_Produktu
     DROP CONSTRAINT CenaProduktu_Produkt;
@@ -76,5 +73,206 @@ DROP TABLE Sprzedaż;
 
 DROP TABLE Zamówienia;
 
--- End of file.
 
+-- tables
+-- Table: Cena_Produktu
+CREATE TABLE Cena_Produktu (
+    Id number(8)  DEFAULT ON NOT NULL,
+    Id_Produktu number(6)  NOT NULL,
+    Cena number(6)  NOT NULL,
+    Od date  NOT NULL,
+    Do date  NOT NULL,
+    CONSTRAINT Cena_Produktu_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Dostawca
+CREATE TABLE Dostawca (
+    Id number(4)  DEFAULT ON NOT NULL,
+    Nazwa varchar2(50)  NOT NULL,
+    CONSTRAINT Dostawca_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Dział
+CREATE TABLE Dział (
+    Id number(3)  DEFAULT ON NOT NULL,
+    Nazwa varchar2(30)  NOT NULL,
+    CONSTRAINT Dział_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Kategoria_Produktu
+CREATE TABLE Kategoria_Produktu (
+    Id number(3)  DEFAULT ON NOT NULL,
+    Nazwa varchar2(50)  NOT NULL,
+    Id_Działu number(3)  NOT NULL,
+    CONSTRAINT Kategoria_Produktu_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Magazyn
+CREATE TABLE Magazyn (
+    Id_Oddziału number(4)  NOT NULL,
+    Id_Produktu number(6)  NOT NULL,
+    Liczba_Produktów number(7)  NOT NULL,
+    Czas timestamp  NOT NULL,
+    CONSTRAINT Magazyn_pk PRIMARY KEY (Id_Oddziału,Id_Produktu)
+) ;
+
+-- Table: Ocena_Od_Klienta
+CREATE TABLE Ocena_Od_Klienta (
+    Id number(1)  DEFAULT ON NOT NULL,
+    Ocena varchar2(30)  NOT NULL,
+    CONSTRAINT Ocena_Od_Klienta_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Oddział
+CREATE TABLE Oddział (
+    Id number(4)  DEFAULT ON NOT NULL,
+    Nazwa varchar2(50)  NOT NULL,
+    Miasto varchar2(25)  NOT NULL,
+    Województwo varchar2(50)  NOT NULL,
+    Ulica_I_Numer varchar2(50)  NOT NULL,
+    CONSTRAINT Oddział_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Pracownik
+CREATE TABLE Pracownik (
+    Id number(5)  DEFAULT ON NOT NULL,
+    Imię varchar2(50)  NOT NULL,
+    Nazwisko varchar2(50)  NOT NULL,
+    CONSTRAINT Pracownik_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Produkt
+CREATE TABLE Produkt (
+    Id number(6)  DEFAULT ON NOT NULL,
+    Id_Kategorii number(3)  NOT NULL,
+    Id_Działu number(3)  NOT NULL,
+    Nazwa varchar2(50)  NOT NULL,
+    CONSTRAINT Produkt_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Produkt_Zamówienie
+CREATE TABLE Produkt_Zamówienie (
+    Id_Produktu number(6)  NOT NULL,
+    Id_Zamówienia number(6)  NOT NULL,
+    Liczba_Sztuk number(4)  NOT NULL,
+    Koszt_Zamówienia_Sztuki number(5)  NOT NULL,
+    CONSTRAINT Produkt_Zamówienie_pk PRIMARY KEY (Id_Produktu,Id_Zamówienia)
+) ;
+
+-- Table: Promocje
+CREATE TABLE Promocje (
+    Id number(6)  DEFAULT ON NOT NULL,
+    Id_Produktu number(6)  NOT NULL,
+    Obniżka_Procentowa number(2)  NULL,
+    Od date  NOT NULL,
+    Do date  NOT NULL,
+    CONSTRAINT Promocje_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Reklamacja
+CREATE TABLE Reklamacja (
+    Id number(2)  DEFAULT ON NOT NULL,
+    Powód varchar2(255)  NOT NULL,
+    CONSTRAINT Reklamacja_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Sprzedaż
+CREATE TABLE Sprzedaż (
+    Id number(12)  DEFAULT ON NOT NULL,
+    Id_Reklamacji number(2)  NULL,
+    Id_Oceny number(1)  NULL,
+    Id_Produktu number(6)  NOT NULL,
+    Id_Pracownika number(5)  NOT NULL,
+    Liczba_Produktów number(4)  NOT NULL,
+    Czas timestamp  NOT NULL,
+    Zysk number(5)  NOT NULL,
+    Numer_Transakcji number(16)  NOT NULL,
+    CONSTRAINT Sprzedaż_pk PRIMARY KEY (Id)
+) ;
+
+-- Table: Zamówienia
+CREATE TABLE Zamówienia (
+    Id number(6)  DEFAULT ON NOT NULL,
+    Id_Oddziału number(4)  NOT NULL,
+    Id_Dostawcy number(4)  NOT NULL,
+    Koszt_Zamówienia number(8,2)  NOT NULL,
+    Data_Zamówienia date  NOT NULL,
+    CONSTRAINT Zamówienia_pk PRIMARY KEY (Id)
+) ;
+
+-- foreign keys
+-- Reference: CenaProduktu_Produkt (table: Cena_Produktu)
+ALTER TABLE Cena_Produktu ADD CONSTRAINT CenaProduktu_Produkt
+    FOREIGN KEY (Id_Produktu)
+    REFERENCES Produkt (Id);
+
+-- Reference: Kategoria_Produktu_Dział (table: Kategoria_Produktu)
+ALTER TABLE Kategoria_Produktu ADD CONSTRAINT Kategoria_Produktu_Dział
+    FOREIGN KEY (Id_Działu)
+    REFERENCES Dział (Id);
+
+-- Reference: Magazyn_Oddział (table: Magazyn)
+ALTER TABLE Magazyn ADD CONSTRAINT Magazyn_Oddział
+    FOREIGN KEY (Id_Oddziału)
+    REFERENCES Oddział (Id);
+
+-- Reference: Magazyn_Produkt (table: Magazyn)
+ALTER TABLE Magazyn ADD CONSTRAINT Magazyn_Produkt
+    FOREIGN KEY (Id_Produktu)
+    REFERENCES Produkt (Id);
+
+-- Reference: ProduktZamówienie_Produkt (table: Produkt_Zamówienie)
+ALTER TABLE Produkt_Zamówienie ADD CONSTRAINT ProduktZamówienie_Produkt
+    FOREIGN KEY (Id_Produktu)
+    REFERENCES Produkt (Id);
+
+-- Reference: ProduktZamówienie_Zamówienia (table: Produkt_Zamówienie)
+ALTER TABLE Produkt_Zamówienie ADD CONSTRAINT ProduktZamówienie_Zamówienia
+    FOREIGN KEY (Id_Zamówienia)
+    REFERENCES Zamówienia (Id);
+
+-- Reference: Produkt_Dział (table: Produkt)
+ALTER TABLE Produkt ADD CONSTRAINT Produkt_Dział
+    FOREIGN KEY (Id_Działu)
+    REFERENCES Dział (Id);
+
+-- Reference: Produkt_KategoriaProduktu (table: Produkt)
+ALTER TABLE Produkt ADD CONSTRAINT Produkt_KategoriaProduktu
+    FOREIGN KEY (Id_Kategorii)
+    REFERENCES Kategoria_Produktu (Id);
+
+-- Reference: Produkt_Promocje (table: Promocje)
+ALTER TABLE Promocje ADD CONSTRAINT Produkt_Promocje
+    FOREIGN KEY (Id_Produktu)
+    REFERENCES Produkt (Id);
+
+-- Reference: Sprzedaż_OcenaOdKlienta (table: Sprzedaż)
+ALTER TABLE Sprzedaż ADD CONSTRAINT Sprzedaż_OcenaOdKlienta
+    FOREIGN KEY (Id_Oceny)
+    REFERENCES Ocena_Od_Klienta (Id);
+
+-- Reference: Sprzedaż_Pracownik (table: Sprzedaż)
+ALTER TABLE Sprzedaż ADD CONSTRAINT Sprzedaż_Pracownik
+    FOREIGN KEY (Id_Pracownika)
+    REFERENCES Pracownik (Id);
+
+-- Reference: Sprzedaż_Produkt (table: Sprzedaż)
+ALTER TABLE Sprzedaż ADD CONSTRAINT Sprzedaż_Produkt
+    FOREIGN KEY (Id_Produktu)
+    REFERENCES Produkt (Id);
+
+-- Reference: Sprzedaż_Reklamacja (table: Sprzedaż)
+ALTER TABLE Sprzedaż ADD CONSTRAINT Sprzedaż_Reklamacja
+    FOREIGN KEY (Id_Reklamacji)
+    REFERENCES Reklamacja (Id);
+
+-- Reference: Zamówienia_Dostawca (table: Zamówienia)
+ALTER TABLE Zamówienia ADD CONSTRAINT Zamówienia_Dostawca
+    FOREIGN KEY (Id_Dostawcy)
+    REFERENCES Dostawca (Id);
+
+-- Reference: Zamówienia_Oddział (table: Zamówienia)
+ALTER TABLE Zamówienia ADD CONSTRAINT Zamówienia_Oddział
+    FOREIGN KEY (Id_Oddziału)
+    REFERENCES Oddział (Id);
+	

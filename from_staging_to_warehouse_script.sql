4eaf15eacfec1f58df5e8b8c54d2a5857d59bd6a
Dayve <theholcombian@gmail.com>
2017-11-28 20:58:54 +0100

    ETL for all fact tables except orders table

--- from_staging_to_warehouse_script.sql
+++ from_staging_to_warehouse_script.sql
@@ -69,16 +69,16 @@ IS
     sumaryczna_kwota NUMBER;
     id_przedzialu_wielkosci NUMBER;
 BEGIN
-	SELECT sum(cena*zbd_staging.Sprzedaż.liczba_produktów) INTO sumaryczna_kwota
-	FROM zbd_staging.cena_produktu
-	LEFT JOIN zbd_staging.Sprzedaż
-	ON zbd_staging.cena_produktu.id_produktu = zbd_staging.Sprzedaż.id_produktu
-	WHERE zbd_staging.Sprzedaż.czas BETWEEN zbd_staging.cena_produktu.od AND zbd_staging.cena_produktu.do
-	AND zbd_staging.Sprzedaż.numer_transakcji = dany_numer_transakcji
-	GROUP BY zbd_staging.Sprzedaż.numer_transakcji;
+    SELECT sum(cena*zbd_staging.Sprzedaż.liczba_produktów) INTO sumaryczna_kwota
+    FROM zbd_staging.cena_produktu
+    LEFT JOIN zbd_staging.Sprzedaż
+    ON zbd_staging.cena_produktu.id_produktu = zbd_staging.Sprzedaż.id_produktu
+    WHERE zbd_staging.Sprzedaż.czas BETWEEN zbd_staging.cena_produktu.od AND zbd_staging.cena_produktu.do
+    AND zbd_staging.Sprzedaż.numer_transakcji = dany_numer_transakcji
+    GROUP BY zbd_staging.Sprzedaż.numer_transakcji;
 
-	SELECT id INTO id_przedzialu_wielkosci FROM zbd_warehouse.wielkosc_transakcji
-	WHERE sumaryczna_kwota >= zbd_warehouse.wielkosc_transakcji.od AND sumaryczna_kwota < zbd_warehouse.wielkosc_transakcji.do;
+    SELECT id INTO id_przedzialu_wielkosci FROM zbd_warehouse.wielkosc_transakcji
+    WHERE sumaryczna_kwota >= zbd_warehouse.wielkosc_transakcji.od_PLN AND sumaryczna_kwota < zbd_warehouse.wielkosc_transakcji.do_PLN;
 
     RETURN id_przedzialu_wielkosci;
 END;
@@ -88,16 +88,16 @@ CREATE OR REPLACE FUNCTION CENA_PRODUKTU_W_TRANSAKCJI
     (dany_numer_transakcji IN NUMBER)
     RETURN NUMBER
 IS
-	cena_produktu NUMBER;
+    cena_produktu NUMBER;
 BEGIN
-	SELECT zbd_staging.cena_produktu.cena INTO cena_produktu
-	FROM zbd_staging.cena_produktu
-	LEFT JOIN zbd_staging.Sprzedaż
-	ON zbd_staging.cena_produktu.id_produktu = zbd_staging.Sprzedaż.id_produktu
-	WHERE zbd_staging.Sprzedaż.czas BETWEEN zbd_staging.cena_produktu.od AND zbd_staging.cena_produktu.do
-	AND zbd_staging.Sprzedaż.numer_transakcji = dany_numer_transakcji;
-	
-	RETURN cena_produktu;
+    SELECT zbd_staging.cena_produktu.cena INTO cena_produktu
+    FROM zbd_staging.cena_produktu
+    LEFT JOIN zbd_staging.Sprzedaż
+    ON zbd_staging.cena_produktu.id_produktu = zbd_staging.Sprzedaż.id_produktu
+    WHERE zbd_staging.Sprzedaż.czas BETWEEN zbd_staging.cena_produktu.od AND zbd_staging.cena_produktu.do
+    AND zbd_staging.Sprzedaż.numer_transakcji = dany_numer_transakcji;
+    
+    RETURN cena_produktu;
 END;
 /
 
@@ -108,10 +108,10 @@ IS
     cena_produktu NUMBER;
     id_kategorii_cenowej NUMBER;
 BEGIN
-	cena_produktu := CENA_PRODUKTU_W_TRANSAKCJI(dany_numer_transakcji);
+    cena_produktu := CENA_PRODUKTU_W_TRANSAKCJI(dany_numer_transakcji);
 
-	SELECT id INTO id_kategorii_cenowej FROM zbd_warehouse.kategoria_cenowa
-	WHERE cena_produktu >= zbd_warehouse.kategoria_cenowa.od_PLN AND cena_produktu < zbd_warehouse.kategoria_cenowa.do_PLN;
+    SELECT id INTO id_kategorii_cenowej FROM zbd_warehouse.kategoria_cenowa
+    WHERE cena_produktu >= zbd_warehouse.kategoria_cenowa.od_PLN AND cena_produktu < zbd_warehouse.kategoria_cenowa.do_PLN;
 
     RETURN id_kategorii_cenowej;
 END;
@@ -123,70 +123,70 @@ CREATE OR REPLACE FUNCTION DOSTAWCA_DANEGO_PRODUKTU
     (p_id_produktu IN NUMBER)
     RETURN NUMBER
 IS
-	id_zamowienia_produktu NUMBER;
-	id_dostawcy_produktu NUMBER;
+    id_zamowienia_produktu NUMBER;
+    id_dostawcy_produktu NUMBER;
 BEGIN
-	SELECT Id_Zamówienia INTO id_zamowienia_produktu FROM Produkt_Zamówienie
-	WHERE Id_Produktu = p_id_produktu;
+    SELECT Id_Zamówienia INTO id_zamowienia_produktu FROM Produkt_Zamówienie
+    WHERE Id_Produktu = p_id_produktu;
 
-	SELECT Id_Dostawcy INTO id_dostawcy_produktu FROM Zamówienia
-	WHERE Id = id_zamowienia_produktu;
+    SELECT Id_Dostawcy INTO id_dostawcy_produktu FROM Zamówienia
+    WHERE Id = id_zamowienia_produktu;
 
-	RETURN id_dostawcy_produktu;
+    RETURN id_dostawcy_produktu;
 END;
 /
 
-CREATE OR REPLACE FUNCTION ZYSK_ZE_SPRZEDAZY
-    (p_id_produktu IN NUMBER,
-	 p_liczba_produktów IN NUMBER
-	 p_numer_transakcji IN NUMBER)
+create or replace FUNCTION ZYSK_ZE_SPRZEDAZY
+    (p_id_Produktu IN NUMBER,
+     p_liczba_produktów IN NUMBER,
+     p_numer_transakcji IN NUMBER,
+     p_Id_Oddzialu IN NUMBER)
     RETURN NUMBER
 IS
-	Liczba_Produktów_Zamówienia NUMBER;
-	Szukany_Koszt_Sztuki NUMBER;
-	
+    Liczba_Produktow_Zamowienia NUMBER;
+    Szukany_Koszt_Sztuki NUMBER;
+    Obecna_Liczba_Produktow NUMBER;
 BEGIN
-	SELECT Liczba_Produktów 
-	INTO Obecna_Liczba_Produktów
-	FROM MAGAZYN
-	WHERE Id_Produktu = p_Id_Produktu
-	AND Id_Oddziału = p_Id_Oddziału
-	ORDER BY date DESC
-	LIMIT 1;
+    SELECT Liczba_Produktów 
+    INTO Obecna_Liczba_Produktow
+    FROM zbd_staging.MAGAZYN
+    WHERE Id_Produktu = p_Id_Produktu
+    AND Id_Oddziału = p_Id_Oddzialu
+    AND rownum = 1
+    ORDER BY czas DESC;
 
-	FOR wiersz IN (SELECT Id FROM zbd_staging.Zamówienia WHERE Id_Oddziału = 	p_Id_Oddziału ORDER BY Data_Zamówienia DESC) LOOP
-		SELECT Liczba_Sztuk, Koszt_Zamówienia_Sztuki INTO Liczba_Produktów_Zamówienia,
-		Szukany_Koszt_Sztuki FROM Produkt_Zamówienie WHERE Id_Zamówienia = wiersz.Id;
-		
-		Obecna_Liczba_Produktów := Obecna_Liczba_Produktów - Liczba_Produktów_Zamówienia;
-		
-		EXIT WHEN Obecna_Liczba_Produktów <= 0;
-	END LOOP;
-	
-	 return (CENA_PRODUKTU_W_TRANSAKCJI(p_numer_transakcji) - Szukany_Koszt_Sztuki) * p_liczba_produktów;
+    FOR wiersz IN (SELECT Id FROM zbd_staging.Zamówienia WHERE Id_Oddziału = p_Id_Oddzialu ORDER BY Data_Zamówienia DESC) LOOP
+        SELECT Liczba_Sztuk, Koszt_Zamówienia_Sztuki INTO Liczba_Produktow_Zamowienia,
+        Szukany_Koszt_Sztuki FROM Produkt_Zamówienie WHERE Id_Zamówienia = wiersz.Id;
+
+        Obecna_Liczba_Produktow := Obecna_Liczba_Produktow - Liczba_Produktow_Zamowienia;
+
+        EXIT WHEN Obecna_Liczba_Produktow <= 0;
+    END LOOP;
+
+    return (CENA_PRODUKTU_W_TRANSAKCJI(p_numer_transakcji) - Szukany_Koszt_Sztuki) * p_liczba_produktów;
 END;
 /
 
 -- Sprzedaż:
-CREATE OR REPLACE PROCEDURE fakty_dla_sprzedazy
+create or replace PROCEDURE fakty_dla_sprzedazy
 IS
-	kwota_sumaryczna_transakcji number;
-	id_obowiazujacej_promocji number;
+    kwota_sumaryczna_transakcji number;
+    id_obowiazujacej_promocji number;
     id_typu_produktu number;
 
     id_kategorii number;
     id_dzialu number;
     nazwa_kategorii varchar2(50);
     nazwa_dzialu varchar2(30);
-    id_typu_produktu number;
 
-	id_nowego_czasu number;
+    id_nowego_czasu number;
 BEGIN
-    FOR wiersz IN (SELECT FROM Id_Reklamacji, Id_Oceny, Id_Produktu, Id_Pracownika, Liczba_Produktów, Czas, Zysk, Numer_Transakcji, Id_Oddziału
+    FOR wiersz IN (SELECT Id_Reklamacji, Id_Oceny, Id_Produktu, Id_Pracownika, Liczba_Produktów, Czas, Zysk, Numer_Transakcji, Id_Oddziału
                    FROM zbd_staging.Sprzedaż) LOOP
 
-		SELECT id INTO id_obowiazujacej_promocji FROM zbd_staging.Promocje
-		WHERE wiersz.Czas between od and do;
+        SELECT id INTO id_obowiazujacej_promocji FROM zbd_staging.Promocje
+        WHERE wiersz.Czas between od and do;
 
 
         SELECT id_kategorii, id_działu INTO id_kategorii, id_dzialu FROM zbd_staging.Produkt WHERE id = wiersz.id_produktu;
@@ -201,15 +201,14 @@ BEGIN
         id_nowego_czasu := DODAJ_CZAS(wiersz.Czas);
 
         INSERT INTO zbd_warehouse.Fakty_Sprzedaz 
-		(Id_Wielkosci_Transakcji, Id_Reklamacji, Id_Promocji, Id_Pracownika,
-		  Id_Produktu, Id_Typu_Produktu, Id_Lokalizacji, Id_Czasu, Id_Kategorii_Cenowej,
-		  Id_Oceny, Id_Dostawcy, Zysk, Liczba_Sztuk)
+        (Id_Wielkosci_Transakcji, Id_Reklamacji, Id_Promocji, Id_Pracownika,
+          Id_Produktu, Id_Typu_Produktu, Id_Lokalizacji, Id_Czasu, Id_Kategorii_Cenowej,
+          Id_Oceny, Id_Dostawcy, Zysk, Liczba_Sztuk)
         VALUES 
-		(ID_PRZEDZIALU_CENY(wiersz.Numer_Transakcji), wiersz.Id_Reklamacji, id_obowiazujacej_promocji, 
-		wiersz.Id_Pracownika, wiersz.Id_Produktu, id_typu_produktu, wiersz.Id_Oddziału, id_nowego_czasu, 
-		ID_KATEGORII_CENOWEJ(wiersz.Numer_Transakcji), wiersz.Id_Oceny, DOSTAWCA_DANEGO_PRODUKTU(wiersz.Id_Produktu),
-		ZYSK_ZE_SPRZEDAZY(wiersz.Id_Produktu, wiersz.Liczba_Produktów, wiersz.Numer_Transakcji), wiersz.Liczba_Produktów);
+        (ID_PRZEDZIALU_CENY(wiersz.Numer_Transakcji), wiersz.Id_Reklamacji, id_obowiazujacej_promocji, 
+        wiersz.Id_Pracownika, wiersz.Id_Produktu, id_typu_produktu, wiersz.Id_Oddziału, id_nowego_czasu, 
+        ID_KATEGORII_CENOWEJ(wiersz.Numer_Transakcji), wiersz.Id_Oceny, DOSTAWCA_DANEGO_PRODUKTU(wiersz.Id_Produktu),
+        ZYSK_ZE_SPRZEDAZY(wiersz.Id_Produktu, wiersz.Liczba_Produktów, wiersz.Numer_Transakcji, wiersz.Id_Oddziału), wiersz.Liczba_Produktów);
     END LOOP;
 END;
 /
-
